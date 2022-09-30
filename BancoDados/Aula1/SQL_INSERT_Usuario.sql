d51aa20e19d7dea78d5d572c4083684b12c18f10
Yasmin <yasminalvesdesouza27@gmail.com>
2022-09-29 22:38:29 -0300

    Aula 2 - Banco de Dados - Queries

---
+++ BancoDados/Aula1/SQL_INSERT_Usuario.sql
@@ -0,0 +1,10 @@
+INSERT INTO usuario (id_pessoa, 
+					login, 
+					senha, 
+					status)
+VALUES  (1,'yasmin', '2016', 'A'),
+		(2,'yasmin', '2016', 'A'),
+        (3,'vic', '2016', 'A'),
+        (4,'morg', '2016', 'A'),
+        (5,'top', '2016', 'A')
+;
