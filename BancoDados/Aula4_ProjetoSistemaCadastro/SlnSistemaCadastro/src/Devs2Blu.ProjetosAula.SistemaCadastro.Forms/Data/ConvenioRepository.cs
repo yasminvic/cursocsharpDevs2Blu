720ca48a7cc45141adb92d342fa750e51444971f
Yasmin <yasminalvesdesouza27@gmail.com>
2022-10-04 22:11:10 -0300

    Models Sistema Cadastro

---
+++ BancoDados/Aula4_ProjetoSistemaCadastro/SlnSistemaCadastro/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Data/ConvenioRepository.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devs2Blu.ProjetosAula.SistemaCadastro.Forms.Data
+{
+    public class ConvenioRepository
+    {
+       /* public MySqlDataReader FetchAll() //pegar tudo
+        {
+
+        }*/
+
+        #region SQLS
+        private const String SQL_SELECT_Convenio = "SELECT * FROM convenio"
+        #endregion
+    }
+}
