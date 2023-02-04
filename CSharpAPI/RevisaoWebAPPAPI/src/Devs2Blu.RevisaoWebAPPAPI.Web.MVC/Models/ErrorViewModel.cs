f4bf7df34eb7c907d6414953cc57ff7cac8b4589
Yasmin <yasminalvesdesouza27@gmail.com>
2023-02-03 22:14:43 -0300

    Implementando arquitetura DDD

---
+++ CSharpAPI/RevisaoWebAPPAPI/src/Devs2Blu.RevisaoWebAPPAPI.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Devs2Blu.RevisaoWebAPPAPI.Web.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
