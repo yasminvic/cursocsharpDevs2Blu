84412b8a7a3567766510cacf4970f44b5badf87a
Yasmin <yasminalvesdesouza27@gmail.com>
2023-02-24 22:47:21 -0300

    Fase 1 - Planning

---
+++ ProjetosIntegrados/ProjetoNotas/src/ProjetoNotas.Domain/Entities/CategoryEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoNotas.Domain.Entities
+{
+    public enum CategoryEnum
+    {
+        Notes = 0,
+        Reminders = 1,
+        Alert = 2,
+
+    }
+}
